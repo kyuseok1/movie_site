02cc6eacb54e538d5b6fdad8437e6e729b8a9cbf
kyuseok1 <jks980308@naver.com>
2023-11-04 18:04:44 +0900

    axios 수정

--- src/routes/Ranking.js
+++ src/routes/Ranking.js
@@ -1,38 +1,37 @@
 
 import React, { useState,useEffect } from 'react';
 import Header from './Header';
+import axios from 'axios';
 
-const Ranking = () => {
+
+function Ranking ()  {
     
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
+  const [data, setData] = useState([]);
   
-  const getMovies = async () => {
+  	
+  	useEffect(() => {
+		const fetchData = async() => {
+          const res = await axios.get(`https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20231103`);
+          return res.data;
+        }	
+        
+        fetchData().then(res => setData(res));
+        
+    }, []);
     
-    const json = await (
-      await fetch(
-        `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20231030`
-      )
-    ).json(); // await를 await로 감싸기
-    setMovies(json.boxOfficeResult.dailyBoxOfficeList);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
   return (
     <div>
+        
+        {console.log(JSON.stringify(data.boxOfficeResult.dailyBoxOfficeList[0]))}
       <Header></Header>
+      
       <div class="container1">
           <div class="inner">
-              <span > Home > 10월 30일 박스오피스</span>
+              <span > Home > 11월 03일 박스오피스</span>
               
           </div>
       </div>
-  {loading ? (
-    <h1>Loading...</h1>
-  ) : (
+  
     <div className='movie-rank-container'>
       <table>
         <tr>
@@ -40,14 +39,14 @@ const Ranking = () => {
           <th>영화명</th>
           <th>개봉일</th>
           <th>관객수</th>
-
         </tr>
-      {movies.map((dailyBoxOfficeList) => (
+        
+        
+        { (data.boxOfficeResult.dailyBoxOfficeList).map((dailyBoxOfficeList) => (
         <tr className='movie-rank' key={dailyBoxOfficeList.rnum}><th>{dailyBoxOfficeList.rank}</th><th>{dailyBoxOfficeList.movieNm}</th><th>{dailyBoxOfficeList.openDt}</th><th>{dailyBoxOfficeList.audiCnt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}명</th></tr>
-      ))}
+        ))} 
       </table>
     </div>
-  )}
 </div>
 
   );
